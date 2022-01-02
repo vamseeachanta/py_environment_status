997aa61f90377929de6a2fac808d9e6b2a51be2a
Vamsee Achanta <vamsee.achanta@aceengineer.com>
2022-01-02 13:47:43 -0600

    wip

--- py_environment_status/tests/test_package.py
+++ py_environment_status/tests/test_package.py
@@ -1,5 +1,9 @@
 import py_environment_status
 
-def test_package_list():
-    py_environment_status.package_list()
 
+def test_package_list():
+  py_environment_status.package_list()
+
+
+def test_example():
+  pass
